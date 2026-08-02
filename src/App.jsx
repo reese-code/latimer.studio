9d50cab433eb405d6e9971413bf6d92d4cda6623
reese-code <reese.coding@gmail.com>
2026-08-02 11:48:40 -0600

    removed curtain

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,17 @@ import AboutPage from './pages/AboutPage'
 import ContactPage from './pages/ContactPage'
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage'
 import useLenis from './hooks/useLenis'
-import PageCurtainTransition from './components/PageCurtainTransition'
 
 export default function App() {
   useLenis()
 
   return (
-    <PageCurtainTransition>
-      {(location) => (
-        <Routes location={location}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/projects/:id" element={<ProjectPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-        </Routes>
-      )}
-    </PageCurtainTransition>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/projects/:id" element={<ProjectPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+    </Routes>
   )
 }
